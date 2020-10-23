c756c55159c34c3e755024d497fef59a4aa2091c
Braden <57928428+Kyronomix@users.noreply.github.com>
2020-10-23 18:32:35 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Reworked for the VORP Community in English
+
+Keys: Use the item in your inventory and hold click to place it.  Use G near a still to open the menu. Use Backspace to cancel menu.
+
+VERSION 1.1 08/20/2020
+
+
+# rgn_moonshiner
+
+- Allows players to build moonshine locations and to brew their own moonshine.
+
+### what to do next
+ 
+- Add item.sql to your database.
+
+Add this to your server.cfg
+```
+ensure progress_bar
+ensure drp_caldero (optional)
+ensure rgn_moonshiner
+```
+ 
+## Acknowledgments 
+ 
+Thanks to:
+ 
+* DArkSoul for developing this in German and everyone over at VORP who helped him out
+
+## Bugs and errors
+ 
+If something is not working, or you find a bug, please reach out to Kyronomix#0908 for English!
+
---
+++ item.sql
@@ -0,0 +1,18 @@
+USE `vorp`;
+
+INSERT INTO `items` (`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES
+                    ('soborno', 'Soborno Alcohol', -1, 1, 'item_standard', 0),
+                    ('water', 'Water', -1, 1, 'item_standard', 1),
+	                ('pear', 'Pear', -1, 1, 'item_standard', 1),
+                    ('currant', 'Currant', -1, 1, 'item_standard', 0),
+                    ('apple', 'Apple', -1, 1, 'item_standard', 1),
+                    ('blackberry', 'Blackberry', -1, 1, 'item_standard', 0),
+                    ('tropicalPunchMash', 'Tropical Punch Mash', -1, 1, 'item_standard', 0),
+                    ('wildCiderMash', 'Wild Cider Mash', -1, 1, 'item_standard', 0),
+                    ('appleCrumbMash', 'Apple Crumb Mash', -1, 1, 'item_standard', 0),
+                    ('tropicalPunchMoonshine', 'Tropical Punch Moonshine', -1, 1, 'item_standard', 0),
+                    ('wildCiderMoonshine', 'Wild Cider Moonshine', -1, 1, 'item_standard', 0),
+                    ('appleCrumbMoonshine', 'Apple Crumb Moonshine', -1, 1, 'item_standard', 0),
+                    ('mp001_p_mp_still02x', 'Moonshine Still', 1, 1, 'item_standard', 1),
+                    ('p_barrelmoonshine', 'Moonshine Barrel', 1, 1, 'item_standard', 1),
+	                ('vanillaFlower', 'Vanilla Flower', -1, 1, 'item_standard', 0);
